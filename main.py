fd696296fb5c238d1ed7156b874ef3854fb6514a
njengu <nenguerra@gmail.com>
2023-01-17 17:13:45 +0000

    Moved test folders to desktop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 #find_files definition
 def find_files(filename, search_path):
@@ -11,25 +12,24 @@ def find_files(filename, search_path):
                result.append(os.path.join(root, fileNames))
    return result
 
-# Step 0 - Create folders by month, by year in desired folder destination
-
+# Setup Step 0 - Create folders by month, by year in desired folder destination
 # destination = "E:\\Phone Media Backups\\2023\\2023-"
-# destination = "Test Files\\2023-"
-#
-# for i in range(1,13):
-#     filename = destination + "{:02d}".format(i)
-#     try:
-#         if os.path.exists(filename):
-#             print("There is already a file there")
-#         else:
-#             os.makedirs(filename)
-#             print("File has been created")
-#     except FileNotFoundError:
-#         print(destination +" was not found")
+destination = "C:\\Users\\Nicanor\\Desktop\\TestFiles\\To\\2023-"
 
-# Step 2 - Search for files named
+for i in range(1,13):
+    filename = destination + "{:02d}".format(i)
+    try:
+        if os.path.exists(filename):
+            print("There is already a file there")
+        else:
+            os.makedirs(filename)
+            print("File has been created")
+    except FileNotFoundError:
+        print(destination +" was not found")
+
+# Step 1 - Search for files named
 # found_files = find_files("202203", "E:\\Phone Media Backups\\2022")
-found_files = find_files("help", "Test Files")
+found_files = find_files("help", "C:\\Users\\Nicanor\\Desktop\\TestFiles\\From")
 print(found_files)
 
 # Step 3 - Copy/Move found files into folders created earlier
