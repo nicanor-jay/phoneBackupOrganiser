308ef0795735ce366075673ec96396f960d104eb
njengu <nenguerra@gmail.com>
2023-01-22 14:48:11 +0000

    Added textbox for year and entry sanitisation. TODO: Add directory explorer

--- sketch.py
+++ sketch.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 
 MONTHOPTIONS = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
@@ -14,24 +15,32 @@ class myGUI:
         self.label = tk.Label(self.root, text="Photo & Video Organiser", font =('Arial', 20))
         self.label.pack(padx=10, pady=10)
 
-        self.startMonth = tk.StringVar(self.root)
-        self.startMonth.set("Select the start month")
-        self.startMonthDropDown = tk.OptionMenu(self.root,self.startMonth, *MONTHOPTIONS)
-        self.startMonthDropDown.pack()
+        self.yearEntry = tk.Text(self.root,  height=1, width=5, font=('Arial', 10))
+        self.yearEntry.pack()
 
-        self.endMonth = tk.StringVar(self.root)
-        self.endMonth.set("Select the end month")
-        self.endMonthDropDown = tk.OptionMenu(self.root,self.endMonth, *MONTHOPTIONS)
-        self.endMonthDropDown.pack()
+        self.startMonthEntry = tk.StringVar(self.root)
+        self.startMonthEntry.set("Select the start month")
+        self.startMonthEntryDropDown = tk.OptionMenu(self.root,self.startMonthEntry, *MONTHOPTIONS)
+        self.startMonthEntryDropDown.pack()
 
-        self.button = tk.Button(self.root, text="Sort", font =('Arial', 10), command=self.test_function)
-        self.button.pack(padx=10,pady=10)
+        self.endMonthEntry = tk.StringVar(self.root)
+        self.endMonthEntry.set("Select the end month")
+        self.endMonthEntryDropDown = tk.OptionMenu(self.root,self.endMonthEntry, *MONTHOPTIONS)
+        self.endMonthEntryDropDown.pack()
+
+        self.sortButton = tk.Button(self.root, text="Sort", font =('Arial', 10), command=self.test_function)
+        self.sortButton.pack(padx=10,pady=10)
 
         self.root.mainloop()
 
     def test_function(self):
-        print(self.startMonth.get())
-        print(self.endMonth.get())
+        try:
+            year = int(self.yearEntry.get('1.0', tk.END))
+            startMonth = int(self.startMonthEntry.get())
+            yearMonth = int(self.endMonthEntry.get())
+        except ValueError:
+            tk.messagebox.showinfo(title="Warning", message="Sorry, please correct the inputs & try again.")
+    def sort_function(self):
         pass
 
 myGUI()
